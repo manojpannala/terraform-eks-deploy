87b11a191305d2116385933ea90ecbc590fea737
manojpannala <manojpannala@outlook.com>
2021-06-11 12:22:03 +0300

    Create eks-cluster iam role

---
+++ iam.tf
@@ -0,0 +1,19 @@
+# Create eks-cluster iam role
+resource "aws_iam_role" "eks_cluster" {
+  name = "eks-cluster"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
